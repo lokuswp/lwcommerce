846272bd4c048c6ee01d561ccc3958365e12e4b4
Chariselyasa123 <ahmad.charis@raharja.info>
2021-12-14 05:30:49 +0700

    Fix path autoload for windows

--- src/autoload.php
+++ src/autoload.php
@@ -1,5 +1,5 @@
 <?php
-if (!defined('WPTEST')) {
+if ( ! defined('WPTEST')) {
     defined('ABSPATH') or die("Direct access to files is prohibited");
 }
 
@@ -12,11 +12,10 @@ spl_autoload_register(function ($classname) {
 
     // var_dump($classname);
 
-    $class      = str_replace('\\', DIRECTORY_SEPARATOR, strtolower($classname));
-    $classpath  = LWPC_PATH  . 'src/includes' . DIRECTORY_SEPARATOR . $class . '.php';
-    $classpath  = str_replace("lokuswp/commerce/", "", $classpath);
-
-    // var_dump($classpath);
+    $class     = str_replace('\\', DIRECTORY_SEPARATOR, strtolower($classname));
+    $classpath = LWPC_PATH.'src/includes'.DIRECTORY_SEPARATOR.$class.'.php';
+    $classpath = str_replace("lokuswp/commerce/", "", $classpath);
+    $classpath = str_replace("lokuswp\\commerce\\", "", $classpath); // fix path for windows
 
     if (file_exists($classpath)) {
         include_once $classpath;
