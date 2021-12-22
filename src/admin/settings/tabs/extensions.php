6e5df6e5e42ab93e7cd17024b01c82b5bf17cc3e
lasida <lasidaziz@gmail.com>
2021-12-22 09:14:58 +0700

    extension screen

--- src/admin/settings/tabs/extensions.php
+++ src/admin/settings/tabs/extensions.php
@@ -1,1 +1,59 @@
-Exstensi LWPCommerce
+<br>
+<div class="column col-12">
+    <div class="filter">
+        <input class="filter-tag" id="tag-0" type="radio" name="filter-radio" checked="" hidden="">
+        <input class="filter-tag" id="tag-1" type="radio" name="filter-radio" hidden="">
+        <input class="filter-tag" id="tag-2" type="radio" name="filter-radio" hidden="">
+        <input class="filter-tag" id="tag-3" type="radio" name="filter-radio" hidden="">
+        <input class="filter-tag" id="tag-4" type="radio" name="filter-radio" hidden="">
+        <div class="filter-nav">
+            <label class="chip" for="tag-0">All</label>
+            <label class="chip" for="tag-1">Advanced</label>
+            <!-- <label class="chip" for="tag-2">Payments</label>
+            <label class="chip" for="tag-3">Utility</label>
+            <label class="chip" for="tag-4">Advanced</label> -->
+        </div>
+        <div class="filter-body columns">
+
+            <div class="column col-4 filter-item" data-tag="tag-1">
+                <div class="card">
+                    <div class="card-header">
+                        <div class="card-title text-bold">Pro</div>
+                        <div class="card-subtitle text-gray">Variant Produk, Kupon</div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="column col-4 filter-item" data-tag="tag-1">
+                <div class="card">
+                    <div class="card-header">
+                        <div class="card-title text-bold">Affiliate</div>
+                        <div class="card-subtitle text-gray">Create you Join Venture</div>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="column col-4 filter-item" data-tag="tag-1">
+                <div class="card">
+                    <div class="card-header">
+                        <div class="card-title text-bold">Elementor Widget</div>
+                        <div class="card-subtitle text-gray">Ecommerce Desginer NoCode</div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+<style>
+    .card {
+        padding: 0;
+
+    }
+
+    .filter-tag {
+        display: none !important;
+    }
+</style>
