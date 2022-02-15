91986f09a926499aae01f941a770aac9b7c16090
Chariselyasa123 <ahmad.charis@raharja.info>
2022-02-15 22:04:42 +0700

    Something somethig

--- lwpcommerce.php
+++ lwpcommerce.php
@@ -2,7 +2,7 @@
 
 /**
  * @wordpress-plugin
- * 
+ *
  * Plugin Name:       LWPCommerce - BETA
  * Plugin URI:        https://lokuswp.id/plugin/lwpcommerce
  * Description:       Jual Beli Online jadi Menyenangkan
@@ -16,10 +16,9 @@
  */
 
 
-
 // Checking Test Env and Direct Access File
-if (!defined('WPTEST')) {
-  defined('ABSPATH') or die("Direct access to files is prohibited");
+if ( ! defined( 'WPTEST' ) ) {
+	defined( 'ABSPATH' ) or die( "Direct access to files is prohibited" );
 }
 
 /**
@@ -28,46 +27,27 @@ if (!defined('WPTEST')) {
  * Rename this for your plugin and update it as you release new versions.
  * Define Constant
  */
-defined('LOKUSWP_VERSION') or define('LOKUSWP_VERSION', '0.5.0');
-defined('LWPC_VERSION') or define('LWPC_VERSION', '0.5.0');
-defined('LWPC_BASE') or define('LWPC_BASE', plugin_basename(__FILE__));
-defined('LWPC_PATH') or define('LWPC_PATH', plugin_dir_path(__FILE__));
-defined('LWPC_URL') or define('LWPC_URL', plugin_dir_url(__FILE__));
-defined('LWPC_STORAGE') or define('LWPC_STORAGE', wp_get_upload_dir()['basedir'] . '/lwpcommerce');
-defined('LWPC_STRING_TEXT') or define('LWPC_STRING_TEXT', '0.0.1'); // String Version
+defined( 'LOKUSWP_VERSION' ) or define( 'LOKUSWP_VERSION', '0.5.0' );
+defined( 'LWPC_VERSION' ) or define( 'LWPC_VERSION', '0.5.0' );
+defined( 'LWPC_BASE' ) or define( 'LWPC_BASE', plugin_basename( __FILE__ ) );
+defined( 'LWPC_PATH' ) or define( 'LWPC_PATH', plugin_dir_path( __FILE__ ) );
+defined( 'LWPC_URL' ) or define( 'LWPC_URL', plugin_dir_url( __FILE__ ) );
+defined( 'LWPC_STORAGE' ) or define( 'LWPC_STORAGE', wp_get_upload_dir()['basedir'] . '/lwpcommerce' );
+defined( 'LWPC_STRING_TEXT' ) or define( 'LWPC_STRING_TEXT', '0.0.1' ); // String Version
 
 // Autoload
 require_once LWPC_PATH . 'src/autoload.php';
 
 
-function lwpc_cart_data_processing($data)
-{
+function lwpc_pro_cart_data_processing( $data ) {
 
-  $item_id = $data['post_id'];
-  $variation_id = $data['variation_id'];
-  $data['price_normal']   = get_post_meta(!empty($variation_id) ? $variation_id : $item_id, '_price_normal', true) ?? '';
-  $data['price_promo']    = get_post_meta(!empty($variation_id) ? $variation_id : $item_id, '_price_promo', true) ?? '';
-  $data['weight']         = get_post_meta(!empty($variation_id) ? $variation_id : $item_id, '_weight', true) ?? '';
-  $data['stock']          = get_post_meta(!empty($variation_id) ? $variation_id : $item_id, '_stock', true) ?? '';
-  $data['stock_unit']     = get_post_meta($item_id, '_stock_unit', true) ?? '';
+	$item_id              = $data['post_id'];
+	$variation_id         = $data['variation_id'];
+	$data['max_purchase'] = get_post_meta( $item_id, '_max_purchase', true ) ?? '';
+	$data['min_purchase'] = get_post_meta( $item_id, '_min_purchase', true ) ?? '';
+	$data['variation']    = $variation_id !== null ? $variation->post_excerpt : '';
 
-  // Not Yet Support Variation
-  $data['calc_price']     = lwpc_get_price( $item_id );
-
-  return $data;
+	return $data;
 }
-add_filter('lokuswp/cart/data', 'lwpc_cart_data_processing');
 
-
-function lwpc_pro_cart_data_processing($data)
-{
-
-  $item_id = $data['post_id'];
-  $variation_id = $data['variation_id'];
-  $data['max_purchase']   = get_post_meta($item_id, '_max_purchase', true) ?? '';
-  $data['min_purchase']   = get_post_meta($item_id, '_min_purchase', true) ?? '';
-  $data['variation']     = $variation_id !== null ? $variation->post_excerpt : '';
-
-  return $data;
-}
-add_filter('lokuswp/cart/data', 'lwpc_pro_cart_data_processing');
+add_filter( 'lokuswp/cart/data', 'lwpc_pro_cart_data_processing' );
