e9eacca76eda6fdfe3dfcbec98c49ec17f22c5ae
Chariselyasa123 <ahmad.charis@raharja.info>
2022-04-19 18:37:53 +0700

    Update: send otp

--- src/includes/hook/func-checkout-tab.php
+++ src/includes/hook/func-checkout-tab.php
@@ -7,12 +7,12 @@
  */
 add_action( "lokuswp/transaction/tab/header", function () {
 	?>
-	<div class="swiper-slide">
+    <div class="swiper-slide">
 		<?php _e( 'Shipping', 'lwcommerce' ); ?>
-	</div>
+    </div>
 	<?php
-},2);
+}, 2 );
 
 add_action( "lokuswp/transaction/tab/content", function () {
 	require_once LWC_PATH . 'src/templates/presenter/checkout/shipping.php';
-},2);
+}, 2 );
