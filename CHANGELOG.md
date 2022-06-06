a58810eeb529084417e9c9a6d7b80b3fddd4997c
lasida <lasidaziz@gmail.com>
2022-06-06 11:59:10 +0700

    version update

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,3 +7,7 @@
 # [v0.0.2-alpha](https://lokuswp.id/berita/rilis-lwcommerce-0-1-0-beta/) (2022-05-10)
 **Added :**
 - Alpha release 2
+- 
+# [v0.0.3-alpha](https://lokuswp.id/berita/rilis-lwcommerce-0-1-0-beta/) (2022-05-10)
+**Added :**
+- Alpha release 3
--- lwcommerce.php
+++ lwcommerce.php
@@ -6,7 +6,7 @@
  * Plugin Name:       LWCommerce (beta)
  * Plugin URI:        https://lokuswp.id/plugin/lwcommerce
  * Description:       Local First eCommerce WordPress
- * Version:           0.0.2-alpha
+ * Version:           0.0.3-alpha
  * Author:            LokusWP
  * Author URI:        https://lokuswp.id/
  * License:           GPL-3.0+
@@ -28,12 +28,12 @@ if ( ! defined( 'WPTEST' ) ) {
  * Rename this for your plugin and update it as you release new versions.
  * Define Constant
  */
-defined( 'LWC_VERSION' ) or define( 'LWC_VERSION', '0.0.2' );
+defined( 'LWC_VERSION' ) or define( 'LWC_VERSION', '0.0.3' );
 defined( 'LWC_BASE' ) or define( 'LWC_BASE', plugin_basename( __FILE__ ) );
 defined( 'LWC_PATH' ) or define( 'LWC_PATH', plugin_dir_path( __FILE__ ) );
 defined( 'LWC_URL' ) or define( 'LWC_URL', plugin_dir_url( __FILE__ ) );
 defined( 'LWC_STORAGE' ) or define( 'LWC_STORAGE', wp_get_upload_dir()['basedir'] . '/lwcommerce' );
-defined( 'LWC_TEXT_VERSION' ) or define( 'LWC_TEXT_VERSION', '0.0.2' ); // String Version
+defined( 'LWC_TEXT_VERSION' ) or define( 'LWC_TEXT_VERSION', '0.0.1' ); // String Version
 
 // Autoload
 require_once dirname( __DIR__ ) . '/lwcommerce/src/autoload.php';
