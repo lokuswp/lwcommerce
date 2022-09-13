602c7adfce3c3a9f12fcf3ded4fd81e8e614449b
Chariselyasa123 <ahmad.charis@raharja.info>
2022-09-13 14:08:06 +0700

    Change: scirpt path

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ cp lwcommerce.php lwcommerce/lwcommerce.php
 cp uninstall.php lwcommerce/uninstall.php
 cp CHANGELOG.md lwcommerce/CHANGELOG.md
 cp LICENSE lwcommerce/LICENSE
-#zip -r ./lwcommerce.zip lwcommerce/*
-#rm -R ./lwcommerce/*
-#rm -R ./lwcommerce
+zip -r ./lwcommerce.zip lwcommerce/*
+rm -R ./lwcommerce/*
+rm -R ./lwcommerce
