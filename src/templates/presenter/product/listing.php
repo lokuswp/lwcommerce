3fe0ef5be54631dd35541f19cb4d97f894f11bb3
lasida <lasidaziz@gmail.com>
2022-11-30 11:48:32 +0700

    fix: isotope filter

--- src/templates/presenter/product/listing.php
+++ src/templates/presenter/product/listing.php
@@ -72,11 +72,14 @@
     }
 </style>
 
+
+<?php if ( $filter == 'category' ) :
+wp_enqueue_script( "isotope" ); ?>
 <script>
     jQuery(document).ready(function () {
 
         if (jQuery.isFunction(jQuery.fn.isotope)) {
-            jQuery('.grid').isotope({
+            jQuery('.isotope-container .grid').isotope({
                 itemSelector: '.grid-item',
                 isOriginLeft: true
             });
@@ -84,16 +87,14 @@
             // filter items on button click
             jQuery('.filter-button-group').on('click', 'li', function () {
                 var filterValue = jQuery(this).attr('data-filter');
-                jQuery('.grid').isotope({filter: filterValue});
-                jQuery('.filter-button-group li').removeClass('active');
+                jQuery(this).closest('.filters').siblings('.grid').isotope({filter: filterValue});
+                jQuery(this).closest('.filters').find('li').removeClass('active');
                 jQuery(this).addClass('active');
             });
         }
     })
 </script>
-
-<?php if ( $filter == 'category' ) : wp_enqueue_script( "isotope" ); ?>
-
+<div class="isotope-container">
     <div class="filters filter-button-group">
         <ul class="scrollmenu">
 
@@ -119,140 +120,137 @@
 
         </ul>
     </div>
-<?php endif; ?>
+	<?php endif; ?>
 
-<div class="lwc-listing row content grid">
+    <div class="lwc-listing row content grid">
 
-	<?php
+		<?php
 
 
-	$args = array(
-		'post_type'      => 'product',
-		'post_status'    => 'publish',
-		'posts_per_page' => $limit,
-		'orderby'        => 'date',
-		'order'          => 'DESC',
-		'cat'            => 'home',
-	);
-
-	if ( ! empty( $category ) ) {
-		$args['tax_query'] = array(
-			array(
-				'taxonomy' => 'product_category',
-				'field'    => 'slug',
-				'terms'    => array( sanitize_key( $category ) )
-			)
+		$args = array(
+			'post_type'      => 'product',
+			'post_status'    => 'publish',
+			'posts_per_page' => $limit,
+			'orderby'        => 'date',
+			'order'          => 'DESC',
+			'cat'            => 'home',
 		);
-	}
 
-	$loop = new WP_Query( $args );
+		if ( ! empty( $category ) ) {
 
-	// Get outside Loop
-	$opt['whatsapp']        = lwp_sanitize_phone( lwp_get_settings( 'lwcommerce', 'store', 'whatsapp' ) );
-	$opt['catalog_mode']    = lwp_get_settings( 'lwcommerce', 'appearance', 'catalog_mode' );
-	$opt['whatsapp_button'] = lwp_get_settings( 'lwcommerce', 'appearance', 'button_whatsapp' );
+			if ( strpos( $category, ',' ) ) {
+				$category = explode( ",", $category );
+			}
 
-	if ( $mode == "catalog" ) {
-		$opt['catalog_mode'] = "on";
-	}
+			$args['tax_query'] = array(
+				array(
+					'taxonomy' => 'product_category',
+					'field'    => 'slug',
+					'terms'    => $category
+				)
+			);
+		}
 
-	while ( $loop->have_posts() ) : $loop->the_post();
+		$loop = new WP_Query( $args );
 
-		$col  = $column == 3 ? 'col-md-4' : 'col-md-3';
-		$size = $column == 3 ? 'lwcommerce-thumbnail-listing--desktop' : 'lwcommerce-thumbnail-listing--mobile';
+		// Get outside Loop
+		$opt['whatsapp']        = lwp_sanitize_phone( lwp_get_settings( 'lwcommerce', 'store', 'whatsapp' ) );
+		$opt['catalog_mode']    = lwp_get_settings( 'lwcommerce', 'appearance', 'catalog_mode' );
+		$opt['whatsapp_button'] = lwp_get_settings( 'lwcommerce', 'appearance', 'button_whatsapp' );
 
-		$terms = get_the_terms( get_the_ID(), 'product_category' );
-		$term  = ( $terms ) ? implode( ' ', wp_list_pluck( $terms, 'slug' ) ) : '';
-		?>
-        <div class="single-content lwc-product-item col-xs-6 col-sm-6 <?= $term ?> grid-item <?= $mobile == true ? '' : $col ?> gutter">
-            <div class="product-image">
-				<?php do_action( "lwcommerce/product/listing/before_image", get_the_ID() ); ?>
+		if ( $mode == "catalog" ) {
+			$opt['catalog_mode'] = "on";
+		}
+
+		while ( $loop->have_posts() ) : $loop->the_post();
+
+			$col  = $column == 3 ? 'col-md-4' : 'col-md-3';
+			$size = $column == 3 ? 'lwcommerce-thumbnail-listing--desktop' : 'lwcommerce-thumbnail-listing--mobile';
+
+			$terms = get_the_terms( get_the_ID(), 'product_category' );
+			$term  = ( $terms ) ? implode( ' ', wp_list_pluck( $terms, 'slug' ) ) : '';
+			?>
+            <div class="single-content lwc-product-item col-xs-6 col-sm-6 <?= $term ?> grid-item <?= $mobile == true ? '' : $col ?> gutter">
+                <div class="product-image">
+					<?php do_action( "lwcommerce/product/listing/before_image", get_the_ID() ); ?>
+                    <a href="<?php echo get_permalink(); ?>">
+                        <img src="<?php echo get_the_post_thumbnail_url( get_the_ID(), $size ); ?>"
+                             alt="<?php the_title(); ?>">
+                    </a>
+
+					<?php // Badge
+					//echo '<div class="lwc-pre-order-badge">Pre Order</div>';
+					?>
+
+					<?php do_action( "lwcommerce/product/listing/after_image", get_the_ID() ); ?>
+                </div>
+
+				<?php do_action( "lwcommerce/product/listing/before_title", get_the_ID() ); ?>
+
                 <a href="<?php echo get_permalink(); ?>">
-                    <img src="<?php echo get_the_post_thumbnail_url( get_the_ID(), $size ); ?>"
-                         alt="<?php the_title(); ?>">
+                    <h3 class="product-name"><?php the_title(); ?></h3>
                 </a>
 
-				<?php // Badge
-				//echo '<div class="lwc-pre-order-badge">Pre Order</div>';
-				?>
+				<?php do_action( "lwcommerce/product/listing/after_title", get_the_ID() ); ?>
 
-				<?php do_action( "lwcommerce/product/listing/after_image", get_the_ID() ); ?>
+                <div class="product-price">
+					<?php echo lwc_get_price_html( get_the_ID() ); ?>
+					<?php do_action( "lwcommerce/product/listing/after_price", get_the_ID() ); ?>
+                </div>
+
+				<?php do_action( "lwcommerce/product/listing/after", get_the_ID(), $opt ); ?>
             </div>
+		<?php
+		endwhile;
 
-			<?php do_action( "lwcommerce/product/listing/before_title", get_the_ID() ); ?>
-
-            <a href="<?php echo get_permalink(); ?>">
-                <h3 class="product-name"><?php the_title(); ?></h3>
-            </a>
-
-			<?php do_action( "lwcommerce/product/listing/after_title", get_the_ID() ); ?>
-
-            <div class="product-price">
-				<?php echo lwc_get_price_html( get_the_ID() ); ?>
-				<?php do_action( "lwcommerce/product/listing/after_price", get_the_ID() ); ?>
-            </div>
-
-			<?php do_action( "lwcommerce/product/listing/after", get_the_ID(), $opt ); ?>
-        </div>
-	<?php
-	endwhile;
-
-	wp_reset_postdata();
-	?>
+		wp_reset_postdata();
+		?>
 
 
-</div>
-
-
-<div class="lwc-listing row <?= $mobile == true ? 'lwp-mobile-first' : '' ?>">
-
-
-    <div class="swiper-container swiper-tabs-nav  swiper-isotope">
-        <div class="swiper-wrapper isotope-filter">
-
-
-        </div>
     </div>
 
-
-    <style>
-
-        .product-image {
-            position: relative;
-        }
-
-        .col-xs-6:nth-child(odd) {
-            padding-right: 0.25rem
-        }
-
-        .col-xs-6:nth-child(even) {
-            padding-left: 0.25rem
-        }
-
-        .lwc-listing .col-xs-6 {
-            margin-bottom: 0.25rem;
-        }
-
-        /* PRO CSS */
-        .lwc-product-badge {
-            padding: 4px 0;
-            text-align: left;
-            font-size: 14px;
-            font-weight: 600;
-            border-radius: 3px;
-            margin-bottom: -8px;
-            color: #a4afb7;
-        }
-
-        .lwc-product-badge.new {
-            color: #00d319;
-        }
-
-        .lwc-product-badge.sale {
-            color: #d30000;
-        }
-    </style>
-
+	<?php if ( $filter == 'category' ) : ?>
 </div>
+<?php endif; ?>
+
+
+<style>
+
+    .product-image {
+        position: relative;
+    }
+
+    .col-xs-6:nth-child(odd) {
+        padding-right: 0.25rem
+    }
+
+    .col-xs-6:nth-child(even) {
+        padding-left: 0.25rem
+    }
+
+    .lwc-listing .col-xs-6 {
+        margin-bottom: 0.25rem;
+    }
+
+    /* PRO CSS */
+    .lwc-product-badge {
+        padding: 4px 0;
+        text-align: left;
+        font-size: 14px;
+        font-weight: 600;
+        border-radius: 3px;
+        margin-bottom: -8px;
+        color: #a4afb7;
+    }
+
+    .lwc-product-badge.new {
+        color: #00d319;
+    }
+
+    .lwc-product-badge.sale {
+        color: #d30000;
+    }
+</style>
+
 
 <?php require_once LWC_PATH . 'src/templates/presenter/checkout/bottom-cart-panel.php'; ?>
